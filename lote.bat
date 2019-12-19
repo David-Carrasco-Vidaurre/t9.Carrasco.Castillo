f1ece4da65c33fa74da44fe4320a9a8d6d5f482a
David-Carrasco-Vidaurre <56795495+david-carrasco-vidaurre@users.noreply.github.com>
2019-12-18 20:46:21 -0500

    se creo el archivo lote.bat

---
+++ lote.bat
@@ -0,0 +1,128 @@
+python Carrasco_David/llamado01.py 15.5 10.2 5.4
+python Carrasco_David/llamado01.py 25 4 6
+python Carrasco_David/llamado01.py 50 20 25
+
+
+python Carrasco_David/llamado02.py "David Carrasco" "Ceviche  " "Mazamorra" 15 14
+python Carrasco_David/llamado02.py "Manuel Lopez" "Bistec  " "3 Leches" 25 2.5
+python Carrasco_David/llamado02.py "David" "Arroz Chaufa   " "Arroz con Huevo" 8.5 6.5
+
+
+python Carrasco_David\llamado03.py 15 15
+python Carrasco_David\llamado03.py 17 20
+python Carrasco_David\llamado03.py 10 10
+
+
+
+python Carrasco_David\llamado04.py 15 5 5
+python Carrasco_David\llamado04.py 100 2.5 10
+python Carrasco_David\llamado04.py 150 4.5 20
+
+
+python Carrasco_David\llamado05.py 15 5
+python Carrasco_David\llamado05.py 40 25
+python Carrasco_David\llamado05.py 25.5 4.3
+
+
+python Carrasco_David\llamado06.py 15.4 5
+python Carrasco_David\llamado06.py 1000 20
+python Carrasco_David\llamado06.py 76.5 15
+
+
+
+python Carrasco_David\llamado07.py 20 0.026 50
+python Carrasco_David\llamado07.py 10 0.005 100
+python Carrasco_David\llamado07.py 5 0.062 25
+
+
+python Carrasco_David\llamado08.py "Hola soy nuevo"
+python Carrasco_David\llamado08.py "Universidad Nacional Pedro Ruiz Gallo"
+python Carrasco_David\llamado08.py "Faculta de Ciencias Fisicas y Matematicas"
+
+
+python Carrasco_David\llamado09.py "UNPRG"
+python Carrasco_David\llamado09.py "David Carrasco Vidaurre"
+python Carrasco_David\llamado09.py "Ingenieria electronica"
+
+
+
+python Carrasco_David\llamado10.py "David" 14 14 14
+python Carrasco_David\llamado10.py "Dayana" 18 12 15
+python Carrasco_David\llamado10.py "Nataly" 11 13 14
+
+
+
+python Carrasco_David\llamado11.py "David@7653"
+python Carrasco_David\llamado11.py "Rosas_blancas"
+python Carrasco_David\llamado11.py "unprg-145"
+
+
+python Carrasco_David\llamado12.py 10 5 2
+python Carrasco_David\llamado12.py 15 10 10
+python Carrasco_David\llamado12.py 18 14 9
+
+python Carrasco_David\llamado13.py 10 2
+python Carrasco_David\llamado13.py 14 9
+python Carrasco_David\llamado13.py 12 3
+
+
+python Carrasco_David\llamado14.py 20 5
+python Carrasco_David\llamado14.py 5 2
+python Carrasco_David\llamado14.py 100 25
+
+
+python Carrasco_David\llamado15.py 20 4
+python Carrasco_David\llamado15.py 10.5 4.8
+python Carrasco_David\llamado15.py 20.64 8.4
+
+
+
+python Carrasco_David\llamado16.py 10 40
+python Carrasco_David\llamado16.py 15.5 7.6
+python Carrasco_David\llamado16.py 11 20
+
+
+python Carrasco_David\llamado17.py 121
+python Carrasco_David\llamado17.py 200
+python Carrasco_David\llamado17.py 444
+
+
+python Carrasco_David\llamado18.py 40
+python Carrasco_David\llamado18.py 35
+python Carrasco_David\llamado18.py 15
+
+python Carrasco_David\llamado19.py "DavidCarrasco" "DavidCarrasco"
+python Carrasco_David\llamado19.py "@7653318" "@7653318"
+python Carrasco_David\llamado19.py "DYn2548" "DYn2548"
+
+python Carrasco_David\llamado20.py 20 2000
+python Carrasco_David\llamado20.py 41 1523
+python Carrasco_David\llamado20.py 16 1300
+
+
+python Carrasco_David\llamado21.py "David" "MASCULINO"
+python Carrasco_David\llamado21.py "Karely" "Femenino"
+python Carrasco_David\llamado21.py "Manuel" "masculino"
+
+
+
+python Carrasco_David\llamado22.py 10.5 20.45 200 10
+python Carrasco_David\llamado22.py 15 25 16.5 18
+python Carrasco_David\llamado22.py 20 40 35.5 26.8
+
+
+
+python Carrasco_David\llamado23.py 10
+python Carrasco_David\llamado23.py 50
+python Carrasco_David\llamado23.py 14
+
+
+
+python Carrasco_David\llamado24.py 10 5
+python Carrasco_David\llamado24.py 20 50
+python Carrasco_David\llamado24.py 18.5 5
+
+
+python Carrasco_David\llamado25.py 25.4 14.5
+python Carrasco_David\llamado25.py 20 10
+python Carrasco_David\llamado25.py 15 5
